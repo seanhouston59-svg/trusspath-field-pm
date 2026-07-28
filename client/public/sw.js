864dcfb40d31f354dc1917efc7d3b3349e8c5433
Sean Houston <seanhouston59@gmail.com>
2026-07-28 10:23:54 +0000

    SW v52: Pre-Construction module (3 tabs, portfolio, 3 PDFs, soft-gate)

--- client/public/sw.js
+++ client/public/sw.js
@@ -14,7 +14,7 @@
  * we'd rather let the browser cache them naturally than risk stale bundles.
  */
 
-const CACHE_VERSION = "trusspath-v51";
+const CACHE_VERSION = "trusspath-v52";
 const APP_SHELL = "/";
 const OFFLINE_FALLBACK = "/";
 
